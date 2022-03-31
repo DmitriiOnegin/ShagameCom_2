7af5876493040e2d48cd142686cd7199b05be2ab
Dmitrii Onegin <dimimimo@mail.ru>
2022-03-31 10:58:21 +0300

    test

---
+++ ShagameCom/View/Enter/TestTest.swift
@@ -0,0 +1,20 @@
+//
+//  TestTest.swift
+//  ShagameCom
+//
+//  Created by Dmitrii Onegin on 31.03.2022.
+//
+
+import SwiftUI
+
+struct TestTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestTest_Previews: PreviewProvider {
+    static var previews: some View {
+        TestTest()
+    }
+}
